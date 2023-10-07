500ad454f4ee0a5dadf449586a933ef962b470a4
Vladyslav Shevyrov <warboyka48@gmail.com>
2023-10-07 21:49:29 +0200

    Create display.test.ts

---
+++ tests/display.test.ts
@@ -0,0 +1,20 @@
+import { Display } from "../src/display.js";
+import { Logger } from "../src/logger.js";
+
+describe("Display", () => {
+  let display;
+
+  beforeEach(() => {
+    display = new Display();
+  });
+
+  describe("Initialization", () => {
+    it("should initialize the borderDrafts array as empty", () => {
+      expect(display.borderDrafts).toEqual([]);
+    });
+
+    it("should initialize the logger instance", () => {
+      expect(display.logger).toBeInstanceOf(Logger);
+    });
+  });
+});
